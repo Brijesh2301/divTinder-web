e69dceccbd9ec26b0d982e338719a2bbd20b7c56
Brijesh2301 <brijeshkushwaha386@gmail.com>
2025-11-10 07:40:40 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
         <li>
           <a className="justify-between">
             Profile
-            <span className="badge">New</span>
+            <span className="badge">New</span> 
           </a>
         </li>
         <li><a>Settings</a></li>
