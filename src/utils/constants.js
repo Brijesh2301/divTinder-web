c62a888bb1853d000bcb7f1668e292fb1dd126e3
Brijesh Kushwaha <brijeshkushwaha386@gmail.com>
2026-04-26 14:51:52 +0530

    constatnt j updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,15 @@
+const getBaseURL = () => {
+  const hostname = location.hostname;
+  
+  if (hostname === "localhost") {
+    return "http://localhost:3000";        // Local
+  } else if (hostname === "15.206.222.88") {
+    return "http://15.206.222.88:3000";   // AWS IP
+  } else {
+    return "http://15.206.222.88:3000";   // Vercel
+  }
+};
 
-const BASE_URL =  location.hostname === "localhost" ? "http://localhost:3000" : "/api";
-
+const BASE_URL = getBaseURL();
 
 export default BASE_URL;
